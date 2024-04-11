3a4a005d4b397eca249c6b95a868ad2d2493dc16
CameronS2005 <73669402+CameronS2005@users.noreply.github.com>
2024-04-11 17:05:36 -0400

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,28 +1,40 @@
 #!/bin/bash
 
-############## WE NEED TO SANITIZE THIS INPUT!!!! 
+##### ADD CONFIG SHIT FOR AUTO REMOVAL OF STORED FOOTAGE AFTER X days or minutes 
+##### (For example on rpi we have ~20gb so we keep about 2 days of footage at a time, so every day we remove the day before last.)
+
+############## THIS INPUT NEEDS SANITIZED POST SOURCE
+### NOT ONLY SANITIZED BUT WE ALSO NEED TO CHECK IF THEY ARE VALID AFTERWARDS!!!!
 
 ### RTSP STREAM CONFIG
-rtsp_auth="USER:PASS" # user:pass
-rtsp_relay="rtsp://$rtsp_auth@192.168.68.124:8554" # rtsp://USERNAME:PASSWORD@RTSPIP:RTSPPORT
+rtsp_auth="USERNAME:PASSWORD" # user:pass ## WILL ADD SUPPORT FOR MULTIPLE RELAYS
+rtsp_ip="RTSPRELAYIP"
+rtsp_port="8554"
+rtsp_relay="rtsp://$rtsp_auth@$rtsp_ip:$rtsp_port" # rtsp://USERNAME:PASSWORD@RTSPIP:RTSPPORT
 all_streams=("driveway" "backyard" "garage") # must coincide with wyze camera name minus the -cam for example these are driveway-cam
-enable_audio=true # currently either all or none doesnt support individual cams (IS ENABLED IT MUST ALSO BE ENABLED ON RTSP RELAY FOR EXAMPLE WYZE BRIDGE)
-#enable_debug=false ## NOT IMPLEMENTED ### INTENDED FOR FFMPEG DEBUG
+#enable_debug=false ## NOT IMPLEMENTED ### INTENDED FOR FFMPEG DEBUG #### THIS OPTION WILL FORCE ffmpeg_args_debug=true FOR ALL CAMS
+#is_pi=true
 
-segment_length_minutes="360" # currently 6 hour segments
+segment_length_minutes="15" # currently half hour segments
+
+### Sanity Check Variables
+critical_disk_left="10" # left gb to consider critical
+#critical_temp="60" # temp in f to consider critical
 
 ### Notification Config (Ntfy)
-api="https://ntfy.sh/"
-#CURRENT_TEMP=$(vcgencmd measure_temp | cut -d'=' -f2 | sed 's/[^0-9.]//g')
+api="https://ntfy.sh/NTFY_TOPIC"
+#CURRENT_TEMP=$(vcgencmd measure_temp | cut -d'=' -f2 | sed 's/[^0-9.]//g') ### THIS COMMAND IS RASPBERRY PI SPECIFIC
 
 ### Directories
-log_dir_root="/etc/rtsp_rec/logs"
+disk="/" # used for checking space left.. for sanity checks...
+log_dir_root="/opt/rtsp_rec/logs"
 main_log_file="$log_dir_root/rtsp_rec_host.log"
-capture_dir_root="/etc/rtsp_rec/captures"
+capture_dir_root="/opt/rtsp_rec/captures"
 
 
-### FFMPEG Config
-ffmpeg_args_audio="-c:v copy -c:a aac -strict experimental"
-ffmpeg_args_no_audio="-c:v copy"
+### FFMPEG Config ### WILL ADD SUPPORT FOR HARDWARE ACCELERATION
+ffmpeg_args="-c:v copy" # this is the variable thats used to record!!! FOR FFMPEG STREAMS!
+#ffmpeg_args_audio="-c:v copy -c:a aac -strict experimental"
+#ffmpeg_args_no_audio="-c:v copy"
 
-ffmpeg_args_debug="" ## THIS NEEDS ADDED!!
+#ffmpeg_args_debug="" ## THIS NEEDS ADDED!!
